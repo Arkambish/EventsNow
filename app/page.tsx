5f553eb9ce3e3e71958ee48569e2f39b18dfd543
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-29 19:21:27 +0530

    feat: event route fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Footer from "@/components/Footer";
 import { formatDate } from "@/util/helper";
@@ -10,55 +9,53 @@ import EventCardDisabled from "@/components/EventCardDisabled";
 
 import { Event } from "./admin/Type";
 
+export interface EventType {
+  _id: string;
+  eventName: string;
+  selectedTab: string;
+  eventLocation: string;
+  eventStartDate: string;
+  startTime: string;
+  duration: string;
+  eventTimeZone: string;
+  description: string;
+  postImageLink: string;
+  organizationId: [string];
+  isPublished: boolean;
+  registerUser: [string];
+  dashboardImage: string;
+  __v: number;
+}
 
-// export interface EventType {
-//   _id: string;
-//   eventName: string;
-//   selectedTab: string;
-//   eventLocation: string;
-//   eventStartDate: string;
-//   startTime: string;
-//   duration: string;
-//   eventTimeZone: string;
-//   description: string;
-//   postImageLink: string;
-//   organizationId: [string];
-//   isPublished: boolean;
-//   registerUser: [string];
-//   dashboardImage: string;
-//   __v: number;
-// }
+async function getOutDateEvent() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/v1/event/outdatedEvents`,
+      { next: { revalidate: 10 } }
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+}
 
-// async function getOutDateEvent() {
-//   try {
-//     const response = await fetch(
-//       `${process.env.NEXT_PUBLIC_URL}/api/v1/event/outdatedEvents`,
-//       { next: { revalidate: 10 } }
-//     );
-//     const data = await response.json();
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return [];
-//   }
-// }
+async function getEvent() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/v1/event/getPublishedEvents`,
+      { next: { revalidate: 10 } }
+    );
+    const event = await response.json();
 
-// async function getEvent() {
-//   try {
-//     const response = await fetch(
-//       `${process.env.NEXT_PUBLIC_URL}/api/v1/event/getPublishedEvents`,
-//       { next: { revalidate: 10 } }
-//     );
-//     const event = await response.json();
-
-//     return event;
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return [];
-//   }
-// }
-
+    return event;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+}
 
 export default async function Home() {
   const data = await getOutDateEvent();
@@ -70,62 +67,61 @@ export default async function Home() {
 
       <HeroSection />
 
+      <EventViewMode event={event} />
+      {data.length !== 0 && (
+        <div className="font-bold text-[30px] md:text-[40px] lg:text-5xl text-[#906953] drop-shadow-lg ms-8">
+          Outdated Events
+        </div>
+      )}
 
-//       <EventViewMode event={event} />
-//       {data.length !== 0 && (
-//         <div className="font-bold text-[30px] md:text-[40px] lg:text-5xl text-[#906953] drop-shadow-lg ms-8">
-//           Outdated Events
-//         </div>
-//       )}
+      <div className="flex flex-wrap ms-12">
+        {data.map((e: any) => (
+          <EventCardDisabled
+            key={e._id}
+            name={e.eventName}
+            img={e.dashboardImage}
+            location={e.selectedTab}
+            date={formatDate(e.eventStartDate)}
+          />
+        ))}
+      </div>
 
-//       <div className="flex flex-wrap ms-12">
-//         {data.map((e: any) => (
-//           <EventCardDisabled
-//             key={e._id}
-//             name={e.eventName}
-//             img={e.dashboardImage}
-//             location={e.selectedTab}
-//             date={formatDate(e.eventStartDate)}
-//           />
-//         ))}
-//       </div>
-
-//       <Footer />
-//     </div>
-//   );
-// }
-
-import React from "react";
-import PaymentModal from "@/components/PaymentModal";
-
-export default function page() {
-  const paymentDetails = {
-    items: "Event",
-    oder_id: "123",
-    currency: "LKR",
-    first_name: "Kasun",
-    last_name: "Chathuranga",
-    email: "ruchith.sg@gmail.com",
-    phone: "0771234567",
-    address: "No.1, Galle Road",
-    city: "Colombo",
-    country: "Sri Lanka",
-  };
-
-  const fullAmount = 500;
-  return (
-    <PaymentModal
-      item={paymentDetails?.items}
-      orderId={paymentDetails?.oder_id}
-      amount={fullAmount}
-      currency={paymentDetails?.currency}
-      first_name={paymentDetails?.first_name}
-      last_name={paymentDetails?.last_name}
-      email={paymentDetails?.email}
-      phone={paymentDetails?.phone}
-      address={paymentDetails?.address}
-      city={paymentDetails?.city}
-      country={paymentDetails?.country}
-    />
+      <Footer />
+    </div>
   );
 }
+
+// import React from "react";
+// import PaymentModal from "@/components/PaymentModal";
+
+// export default function page() {
+//   const paymentDetails = {
+//     items: "Event",
+//     oder_id: "123",
+//     currency: "LKR",
+//     first_name: "Kasun",
+//     last_name: "Chathuranga",
+//     email: "ruchith.sg@gmail.com",
+//     phone: "0771234567",
+//     address: "No.1, Galle Road",
+//     city: "Colombo",
+//     country: "Sri Lanka",
+//   };
+
+//   const fullAmount = 500;
+//   return (
+//     <PaymentModal
+//       item={paymentDetails?.items}
+//       orderId={paymentDetails?.oder_id}
+//       amount={fullAmount}
+//       currency={paymentDetails?.currency}
+//       first_name={paymentDetails?.first_name}
+//       last_name={paymentDetails?.last_name}
+//       email={paymentDetails?.email}
+//       phone={paymentDetails?.phone}
+//       address={paymentDetails?.address}
+//       city={paymentDetails?.city}
+//       country={paymentDetails?.country}
+//     />
+//   );
+// }
