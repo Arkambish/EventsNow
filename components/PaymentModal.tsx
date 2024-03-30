736340ee982a2581b34f65dbb279a2e54d0abd07
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-03-30 10:56:22 +0530

    feat: feat/add payment buttton
    \

--- components/PaymentModal.tsx
+++ components/PaymentModal.tsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 import axios from "axios";
 import Script from "next/script";
 import Image from "next/image";
+import { error } from "@/util/Toastify";
 
 declare global {
   interface Window {
@@ -21,7 +22,8 @@ const PaymentModal = (props: any) => {
   const name = props.item;
   const amount = props.amount;
   const merchantId = "1226307";
-  const merchantSecret = "MjY0MDQ5OTc3NTIyNDg2NDk2OTUyMzU2MDY1OTcxMzYyMTEyODYxMA==";
+  const merchantSecret =
+    "MjY0MDQ5OTc3NTIyNDg2NDk2OTUyMzU2MDY1OTcxMzYyMTEyODYxMA==";
   const currency = props.currency || "LKR";
 
   const hashedSecret = crypto
@@ -82,11 +84,11 @@ const PaymentModal = (props: any) => {
       };
 
       window.payhere.onDismissed = function onDismissed() {
-        console.log("asdf");
+        error("Payment dismissed");
       };
 
-      window.payhere.onError = function onError(error: string) {
-        console.log("asdf");
+      window.payhere.onError = function onError(e: string) {
+        error(e);
       };
     };
     scriptRef.current = script;
@@ -248,18 +250,10 @@ const PaymentModal = (props: any) => {
 
       <button
         onClick={pay}
-        className="flex button xl:w-72 w-64 xl:h-16 h-12  bg-[#D47151] rounded-2xl items-center xl:px-4  "
+        className="flex button w-20 p-[1px] bg-[#D47151] rounded-2xl items-center  "
       >
-        <div className=" w-10 h-8 mt-2 ml-2 xl:ml-0">
-          <Image
-            src="https://res.cloudinary.com/dpk9utvby/image/upload/v1710478589/ew/tecmf69jzdyv2sn22saa.svg"
-            alt="print"
-            width={32}
-            height={32}
-          />
-        </div>
         <div className="font-medium xl:text-lg text-md text-white text-left leading-tight ml-4">
-          Buy tickets
+          Pay Now
         </div>
       </button>
     </>
