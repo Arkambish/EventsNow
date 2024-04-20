dd68ecb59daec313d0c9aee2d3a0db99c625ad5b
ruchith <124671914+ruchithx@users.noreply.github.com>
2024-04-20 23:57:47 +0530

    fix: comment ticket card

--- app/event/host/[id]/components/TicketCard.tsx
+++ app/event/host/[id]/components/TicketCard.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import { FiPlusCircle } from "react-icons/fi";
 import { MdDeleteOutline } from "react-icons/md";
 
-
-
 import { TicketArray } from "./HostSideBar";
 
 interface TicketMockupProps {
@@ -30,34 +28,48 @@ export default function TicketCard({
   ticketArray,
   type,
 }: TicketMockupProps) {
+  const [visible, setVisible] = useState(false);
 
-  const [visible , setVisible] = useState(false);
+  // const updateQuantity = (): void => {
+  //   setTotalPrice(totalPrice + price);
 
-    const updateQuantity = (): void => {
-        setVisible(true);
-        setTotalPrice(totalPrice+price);
-        setTicketArray((prev) => [...prev, type]);
-    }
+  // const ticket = {
+  //   typeId,
+  //   type,
+  // };
 
-    const removeQuantity = (): void => {
-      totalPrice !== 0? setTotalPrice(totalPrice - price):setTotalPrice(0);
-      setTicketArray((prevTicketArray) => {
-        const updatedTicketArray = [...prevTicketArray];
-        const ticketIndex = updatedTicketArray.indexOf(type);
-        if (ticketIndex !== -1) {
-          // Decrement quantity if the ticket exists
-          updatedTicketArray[ticketIndex] = updatedTicketArray[ticketIndex] === "1" ? "" : String(Number(updatedTicketArray[ticketIndex]) - 1);
-        }
-        return updatedTicketArray;
-      });
+  //   setTicketArray((prev) => [...prev, ticket]);
+  // };
+
+  const updateQuantity = (): void => {
+    const ticket = {
+      typeId,
+      type,
     };
+    setVisible(true);
+    setTotalPrice(totalPrice + price);
+    setTicketArray((prev) => [...prev, ticket]);
+  };
 
+  const removeQuantity = (): void => {
+    //   totalPrice !== 0 ? setTotalPrice(totalPrice - price) : setTotalPrice(0);
+    //   setTicketArray((prevTicketArray) => {
+    //     const updatedTicketArray = [...prevTicketArray];
+    //     const ticketIndex = updatedTicketArray.indexOf(type);
+    //     if (ticketIndex !== -1) {
+    //       // Decrement quantity if the ticket exists
+    //       updatedTicketArray[ticketIndex] =
+    //         updatedTicketArray[ticketIndex] === "1"
+    //           ? ""
+    //           : String(Number(updatedTicketArray[ticketIndex]) - 1);
+    //     }
+    //     return updatedTicketArray;
+    //   });
+  };
 
   return (
     <div className=" rounded-[10px] border-2 border-[#E2E2E2] pb-4">
-      
       <div className=" w-64 h-60 p-4 overflow-hidden object-cover">
-
         <Image
           src={image}
           width={250}
@@ -67,45 +79,43 @@ export default function TicketCard({
         />
       </div>
       <div className="px-4 flex space-x-12">
-
         <div className="space-y-2">
-          <div className="text-black text-xl font-bold text-start">
-            {type}
-          </div>
+          <div className="text-black text-xl font-bold text-start">{type}</div>
           <div className="text-black text-base text-start font-normal ">
             Rs {price} /=
           </div>
 
-          {visible && (
-            <div className="text-sm font-normal" >
-              Quantity : {ticketArray.indexOf(type) !== -1 ? ticketArray.filter(item => item === type).length : 0}
+          {/* {visible && (
+            <div className="text-sm font-normal">
+              Quantity :{" "}
+              {ticketArray.indexOf(type) !== -1
+                ? ticketArray.filter((item) => item === type).length
+                : 0}
             </div>
-          )}
-
+          )} */}
         </div>
-
 
         <div className="space-y-2 pt-2">
-            <button 
-            className=" w-24 rounded border-[1px] border-[#37A234] px-2 my-auto text-sm font-semibold text-[#37A234] flex  gap-2 " 
-            onClick={updateQuantity}>
-              <div className="py-1">
-                <FiPlusCircle />
-              </div> 
-                Add
-            </button>
+          <button
+            className=" w-24 rounded border-[1px] border-[#37A234] px-2 my-auto text-sm font-semibold text-[#37A234] flex  gap-2 "
+            onClick={updateQuantity}
+          >
+            <div className="py-1">
+              <FiPlusCircle />
+            </div>
+            Add
+          </button>
 
-            <button 
-            className="w-24 rounded border-[1px] border-[#A23434] px-2 my-auto text-sm font-semibold text-[#A23434] flex  gap-2 " 
-            onClick={removeQuantity}>
-              <div className="py-1">
-                <MdDeleteOutline />
-              </div>     
-                Remove
-            </button>  
-
+          <button
+            className="w-24 rounded border-[1px] border-[#A23434] px-2 my-auto text-sm font-semibold text-[#A23434] flex  gap-2 "
+            onClick={removeQuantity}
+          >
+            <div className="py-1">
+              <MdDeleteOutline />
+            </div>
+            Remove
+          </button>
         </div>
-
       </div>
     </div>
   );
